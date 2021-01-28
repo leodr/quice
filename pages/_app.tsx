af0ba860beb394fb0f7394f85a34038131554f86
Leo Driesch <leo.driesch@gmail.com>
2021-01-28 19:48:49 +0100

    :wrench: Set up tailwind

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,16 @@
 import type { AppProps } from "next/app";
+import Head from "next/head";
+import "tailwindcss/tailwind.css";
 
 function CustomApp({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />;
+	return (
+		<>
+			<Head>
+				<link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+			</Head>
+			<Component {...pageProps} />
+		</>
+	);
 }
 
 export default CustomApp;
