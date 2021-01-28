a13cc76d56ede6738ad7ae49bfce336646926368
Leo Driesch <leo.driesch@gmail.com>
2021-01-28 20:29:02 +0100

    :sparkles: Add favicon and app icons

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,6 +7,28 @@ function CustomApp({ Component, pageProps }: AppProps) {
 		<>
 			<Head>
 				<link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+				<link
+					rel="apple-touch-icon"
+					sizes="180x180"
+					href="/apple-touch-icon.png"
+				/>
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="32x32"
+					href="/favicon-32x32.png"
+				/>
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="16x16"
+					href="/favicon-16x16.png"
+				/>
+				<link rel="manifest" href="/site.webmanifest" />
+				<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#ee254c" />
+				<meta name="apple-mobile-web-app-title" content="Quice" />
+				<meta name="application-name" content="Quice" />
+				<meta name="theme-color" content="#ffffff" />
 			</Head>
 			<Component {...pageProps} />
 		</>
