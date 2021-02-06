2985a7be86cab71ed5e3fc78671430ae4cc22e64
Leo Driesch <leo.driesch@gmail.com>
2021-02-06 01:45:43 +0100

    :lock: Throw error for forms that do not take submissions

--- api/submission/submission.service.ts
+++ api/submission/submission.service.ts
@@ -1,4 +1,4 @@
-import { Dependencies, Injectable } from "@nestjs/common";
+import { BadRequestException, Dependencies, Injectable } from "@nestjs/common";
 import { FirebaseService } from "api/firebase/firebase.service";
 import firebase from "firebase-admin";
 import { JsonObject } from "type-fest";
@@ -11,11 +11,24 @@ export class SubmissionService {
 	async create(formId: string, data: JsonObject) {
 		const firestore = this.firebaseService.firestore;
 
+		const formDoc = await firestore.collection("forms").doc(formId).get();
+
+		if (!formDoc.exists) {
+			throw new BadRequestException(
+				`A form with the id \`${formId}\` does not exist.`
+			);
+		}
+
+		if (!formDoc.data()?.allowSubmissions) {
+			throw new BadRequestException(`Submissions were disabled for this form.`);
+		}
+
 		await firestore.collection("submissions").add({
 			createdAt: firebase.firestore.FieldValue.serverTimestamp(),
 			data,
 			formId,
 			readAt: null,
+			done: false,
 		});
 	}
 }
